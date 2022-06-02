d991cff10156a3a32c59d4b0604b11d02e3d88c6
MdAzeen <azeenmd4@gmail.com>
2022-06-03 00:28:02 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Hello React
-    </div>
+     <Switch>
+       <Route exact path="/">
+         This is homepage
+       </Route>
+        <Route exact path="/starred">This is starred</Route>
+       <Route>
+         This is 404 error
+       </Route>
+     </Switch>
   );
 }
 
