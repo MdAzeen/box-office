a21552cb1d52f345ccbc2a6f197d2587e42822f3
MdAzeen <azeenmd4@gmail.com>
2022-06-06 01:55:27 +0530

    added loading and error message to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,39 @@
-import React,{useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 function Show() {
-    const {id}=useParams();
-     const [show,setShow]=useState(null);
-    useEffect(()=>{
-       apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-           setShow(results);
-       })
-    },[id])
-    console.log(show);
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  return (
-    <div>
-      This is show page
-    </div>
-  )
+  useEffect(() => {
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+  console.log(show);
+
+  if (isLoading) return <div>Data is being loaded</div>;
+  if (error) return <div>Error occured: {error}</div>;
+
+  return <div>This is show page</div>;
 }
 
-export default Show
-
+export default Show;
